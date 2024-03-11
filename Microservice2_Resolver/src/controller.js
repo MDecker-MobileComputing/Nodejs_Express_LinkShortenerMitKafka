3722d2c5d4155f641f260e5eeb288f0f02e0fd99
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-03-11 13:12:21 +0100

    MS2, controller.js: horizontale Formatierung beim Template-Aufruf verbessert

--- Microservice2_Resolver/src/controller.js
+++ Microservice2_Resolver/src/controller.js
@@ -19,41 +19,43 @@ export function routenRegistrieren(app) {
 
 
 /**
- * Funktion für HTTP-GET-Request zum Auflösen eines Kurzlinks. 
+ * Funktion für HTTP-GET-Request zum Auflösen eines Kurzlinks.
  * Gibt eine mit Nunjucks gerenderte HTML-Seite zurück.
- * 
+ *
  * @param {*} request Request-Objekt von Express.js, aus dem der
  *                    Pfad-Parameter `kuerzel` ausgelesen wird
- * 
+ *
  * @param {*} response Response-Objekt von Express.js, in das
  *                     der HTTP-Status-Code sowie die gerenderte
- *                     HTML-Seite von der Template-Engine geschrieben 
+ *                     HTML-Seite von der Template-Engine geschrieben
  *                     wird
  */
 function getKuerzel(request, response) {
 
     const kuerzel = request.params.kuerzel.trim();
 
-    
-    const ergObjekt = shortlinkAufloesen(kuerzel); // *** Service-Funktion aufrufen ***
+    // *** Service-Funktion aufrufen ***
+    const ergObjekt = shortlinkAufloesen(kuerzel);
 
     if (!ergObjekt.url) {
 
         response.status(404); // Not found
+
         response.render("nicht_gefunden", {
-            titel: "Fehler",
-            kuerzel: kuerzel
+            titel   : "Fehler",
+            kuerzel : kuerzel
         });
-    
+
     } else {
 
         response.status(200); // OK
-        response.render("gefunden", {
-            titel: `Shortlink "${kuerzel}" aufgelöst`,
-            url         : ergObjekt.url,
-            beschreibung: ergObjekt.beschreibung,
-            erstellt_am : ergObjekt.erstellt_am,
-            geaendert_am: ergObjekt.geaendert_am
+
+        response.render("gefundenx", {
+            titel        : `Shortlink "${kuerzel}" aufgelöst`,
+            url          : ergObjekt.url,
+            beschreibung : ergObjekt.beschreibung,
+            erstellt_am  : ergObjekt.erstellt_am,
+            geaendert_am : ergObjekt.geaendert_am
         });
     }
 }
